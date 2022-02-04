0ec237bb6666aa964b96704fefa23f165ce4fea1
mohammedelserwy <80222232+mohammedelserwy@users.noreply.github.com>
2022-02-04 23:18:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,62 @@
-import React, {Component} from 'react';
-import {
-  I18nManager,
-  KeyboardAvoidingView,
-  Modal,
-  Platform,
-  View,
-} from 'react-native';
-import {CommonActions, NavigationContainer} from '@react-navigation/native';
-import {Provider} from 'react-redux';
-import RouterNavigator from './RouterNavigator';
-import {navigationRef, isReadyRef, reset} from './NavigationActions';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {changeLng} from './Config';
-import {Spinner, VectorIcon} from './components/assets/UIComponents';
-import Store from './store';
 
-function AppContainer(props) {
-  return (
-    <Provider store={Store}>
-        <NavigationContainer
-          ref={navigationRef}
-          onReady={() => {
-            isReadyRef.current = true;
-          }}>
-          <KeyboardAvoidingView
-            style={{flex: 1}}
-            behavior={Platform.OS === 'ios' ? 'padding' : null}>
-            <RouterNavigator  />
-          </KeyboardAvoidingView>
-        </NavigationContainer>
-    </Provider>
-  );
-}
+ 
+import React, { Component } from 'react';
+import { Image, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { firstPageStyles } from './components/assets/styles/Styles';
 class App extends Component {
-  state = {lng: false, lang: '', loading: false};
-  async componentDidMount() {
-    let lang = await AsyncStorage.getItem('language');
-    if (lang) {
-      this.setState({lng: true, lang: lang});
-      changeLng(lang);
-    } else {
-      this.setState({lng: true});
-    }
-    Store.subscribe(() => {
-      const storeState = Store.getState();
-      if (storeState.mainR.loading) {
-        this.setState({loading: true});
-      } else {
-        this.setState({loading: false});
-      }
-    });
-  }
   render() {
-    const {lng, lang, loading} = this.state;
-    if (lng) {
-      return (
-        <View style={{flex: 1}}>
-          <AppContainer lang={lang} />
-          {loading ? <Spinner /> : null}
+    return (
+      <View style={firstPageStyles.pageView}>
+        <TouchableOpacity>
+          <View style={firstPageStyles.itemView}>
+            <Image source={require('./components/assets/images/iconTwo.png')} />
+            <Text style={firstPageStyles.seventhText}>نسيت كلمة المرور </Text>
+          </View>
+        </TouchableOpacity>
+
+        <View style={firstPageStyles.firstView}>
+          <View style={firstPageStyles.secondView}>
+            <Text style={firstPageStyles.firstText}>
+              من فضلك أدخل الكود المرسل إليك
+            </Text>
+            <View style={firstPageStyles.imageView}>
+              <Image source={require('./components/assets/images/phone.png')} />
+
+            </View>
+
+            <View style={firstPageStyles.lineView}>
+              <View >
+                <Text style={firstPageStyles.firstLine}>4</Text>
+              </View>
+
+            </View>
+            <View style={firstPageStyles.rowView}>
+
+
+              <View style={firstPageStyles.secondLineView}></View>
+              <View style={firstPageStyles.secondLineView}></View>
+              <View style={firstPageStyles.secondLineView}></View>
+              <View style={firstPageStyles.firstLineView}></View>
+            </View>
+
+            <View style={firstPageStyles.messageView}>
+              <TouchableOpacity>
+                <Text style={firstPageStyles.messageText}>إعادة إرسال</Text>
+                <View style={firstPageStyles.messageLineView}></View>
+
+              </TouchableOpacity>
+            </View>
+
+          </View>
+          <TouchableOpacity style={firstPageStyles.bottom}>
+            <View style={firstPageStyles.thirdView}>
+              <Text style={firstPageStyles.fifthText}>إدخال الكود</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-      );
-    } else {
-      return <View />;
-    }
+      </View>
+    );
   }
 }
-
 export default App;
+
